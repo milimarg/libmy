9456a9da6839575ac449e30e2c99fe3edc4fdc6e
Florent GUITTRE <florent.guittre@epitech.eu>
2023-01-25 16:08:08 +0100

    remove binaries ; fix my_strcpy & my_strncmp

--- my/my_strcpy.c
+++ my/my_strcpy.c
@@ -7,6 +7,9 @@
 
 char *my_strcpy(char *dest, char const *src)
 {
-    for (int i = 0; src[i] != '\0'; dest[i++] = src[i])
+    int i = 0;
+
+    for (; src[i] != '\0'; i++)
+        dest[i] = src[i];
     return (dest);
 }
--- my/my_strncmp.c
+++ my/my_strncmp.c
@@ -5,11 +5,16 @@
 ** my_strncmp
 */
 
+#include "libmy.h"
+
 int my_strncmp(char *s1, char const *s2, int n)
 {
+    int s1_len = my_strlen(s1);
+    int s2_len = my_strlen(s2);
+
     for (int i = 0; s1[i] != '\0' && i < n; i++) {
-        if (*s1 != *s2)
-            return (1);
+        if (s1[i] != s2[i])
+            return ((s1_len < s2_len) ? -1 : 1);
     }
     return (0);
 }
