47b390fc17bcd72a20363acfcc7a1470cc52614a
Florent GUITTRE <florent.guittre@epitech.eu>
2023-01-25 17:08:52 +0100

    add my_strcmp

---
+++ my/my_strcmp.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** MyHunter
+** File description:
+** my_strcmp.c
+*/
+
+#include "libmy.h"
+
+int my_strcmp(const char *s1, const char *s2)
+{
+    int s1_len = my_strlen(s1);
+    int s2_len = my_strlen(s2);
+
+    for (int i = 0; i < s1_len || i < s2_len; i++)
+        if (s1[i] != s2[i])
+            return ((s1_len < s2_len) ? -1 : 1);
+    return (0);
+}
